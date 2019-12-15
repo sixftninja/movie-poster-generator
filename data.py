41ff85a4132a4152d251412a38dec9e831b1af6f
siddpatny <siddhant.it16@mnnit.ac.in>
2019-12-15 16:02:51 -0500

    train test split fix

--- data.py
+++ data.py
@@ -13,11 +13,11 @@ import numpy as np
 from torch.utils.data.sampler import SubsetRandomSampler
 
 class textImageDataset(torch.utils.data.Dataset):
-    def __init__(self, root='/scratch/ans698/combined_dataset/poster', transform=None,text_path = 'scratch/sp5331/CV/doc2vecEmbeddings.p'):
+    def __init__(self, root='/scratch/ans698/combined_dataset/poster',imageFiles=[], transform=None, text_path = 'scratch/sp5331/CV/doc2vecEmbeddings.p'):
         # super(textImageDataset, self).__init__(root,transform)
         self.transform = transform
         self.root = root
-        self.imageFiles = [f for f in sorted(os.listdir(self.root)) if os.path.isfile(f)]
+        self.imageFiles = imageFiles
         self.embeddings = pickle.load(open(text_path,'rb'))
         # print(len(self.embeddings))
         # print(self.embeddings[0])
@@ -51,35 +51,39 @@ data_transform = transforms.Compose([
             transforms.ToTensor()
         ])
 
-transformed_dataset = textImageDataset(root='/scratch/ans698/combined_dataset/poster',
-                                           text_path = '/scratch/sp5331/CV/doc2vecEmbeddings.p',
-                                           transform = data_transform
-                                           )
 
+imageFiles = [f for f in sorted(os.listdir('/scratch/ans698/combined_dataset/poster'))]
+
+transformed_dataset = textImageDataset(root='/scratch/ans698/combined_dataset/poster',
+    imageFiles = imageFiles,
+    text_path = '/scratch/sp5331/CV/doc2vecEmbeddings.p',
+    transform = data_transform
+    )
 # dataloader = DataLoader(transformed_dataset, batch_size=64,
                         # shuffle=True, num_workers=4)
-
+dataset_size = len(imageFiles)
 validation_split = .2
 random_seed = 42
 batch_size = 64
 num_workers = 4
 shuffle_dataset = True
-dataset_size = len([f for f in sorted(os.listdir('/scratch/ans698/combined_dataset/poster')) if os.path.isfile(f)])
+
+print(dataset_size)
+
 indices = list(range(dataset_size))
 split = int(np.floor(validation_split * dataset_size))
 if shuffle_dataset :
     np.random.seed(random_seed)
     np.random.shuffle(indices)
 train_indices, val_indices = indices[split:], indices[:split]
+print(train_indices)
 
 # Creating PT data samplers and loaders:
 train_sampler = SubsetRandomSampler(train_indices)
 valid_sampler = SubsetRandomSampler(val_indices)
 
-train_loader = DataLoader(transformed_dataset, batch_size=batch_size, 
-                                           sampler=train_sampler,num_workers = num_workers)
-validation_loader = DataLoader(transformed_dataset, batch_size=batch_size,
-                                                sampler=valid_sampler)
+train_loader = DataLoader(transformed_dataset, batch_size=batch_size, sampler=train_sampler,num_workers = num_workers)
+validation_loader = DataLoader(transformed_dataset, batch_size=batch_size, sampler=valid_sampler)
 
 print('dataloader')
 for i, (img,text) in enumerate(train_loader):
